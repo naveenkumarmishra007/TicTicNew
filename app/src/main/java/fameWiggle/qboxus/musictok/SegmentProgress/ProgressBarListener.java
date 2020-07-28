a3bf4c7993de0826cef440604ffaaf2e5efc84e2
Naveen <naveen.mishra@gortnm.in>
2020-07-28 11:08:29 +0530

    Initial commit

---
+++ app/src/main/java/fameWiggle/qboxus/musictok/SegmentProgress/ProgressBarListener.java
@@ -0,0 +1,10 @@
+package fameWiggle.qboxus.musictok.SegmentProgress;
+
+/**
+ * Created by AQEEL on 3/26/2019.
+ */
+
+public interface  ProgressBarListener {
+
+    void TimeinMill(long mills);
+}
---
+++ app/src/main/java/fameWiggle/qboxus/musictok/SimpleClasses/Fragment_Data_Send.java
@@ -0,0 +1,6 @@
+package fameWiggle.qboxus.musictok.SimpleClasses;
+
+public interface Fragment_Data_Send {
+
+    void onDataSent(String yourData);
+}
